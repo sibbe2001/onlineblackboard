be59bd2c929c25e01008cd6ad6decd3cbe3ac25c
sibbe2001 <sibbe2001@gmail.com>
2021-04-19 17:43:10 +0200

    fist css impression

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
     res.render('home')
 })
 
+router.get('/About', (req, res) => {
+    res.render('about')
+})
+
+router.get('/Impressum', (req, res) => {
+    res.render('impressum')
+})
+
+router.get('/Login', (req, res) => {
+    res.render('login')
+})
+
 module.exports = router
